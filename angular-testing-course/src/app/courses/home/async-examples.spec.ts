cee670e67a46f295fdcb66fb4751225c1ba51dbb
Hadi Masoumi <hadi.masoumi@kalemyazilim.com>
2023-11-26 21:44:35 +0300

    33. Testing Asynchronous Code with fakeAsync - Full Example

--- angular-testing-course/src/app/courses/home/async-examples.spec.ts
+++ angular-testing-course/src/app/courses/home/async-examples.spec.ts
@@ -1,4 +1,4 @@
-import { fakeAsync, flush, flushMicrotasks } from '@angular/core/testing';
+import { fakeAsync, flush, flushMicrotasks, tick } from '@angular/core/testing';
 
 describe('Async Testing Examples', () => {
   it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
@@ -44,5 +44,25 @@ describe('Async Testing Examples', () => {
     flushMicrotasks();
     console.log('Running test assertions');
     expect(test).toBeTruthy();
+  }));
+
+  it('Asynchronous test example - Promises + setTimeout()', fakeAsync(() => {
+    let counter = 0;
+    Promise.resolve().then(() => {
+      counter += 10;
+      setTimeout(() => {
+        counter += 1;
+      }, 1000);
+    });
+    expect(counter).toBe(0);
+
+    flushMicrotasks();
+    expect(counter).toBe(10);
+
+    tick(500);
+    expect(counter).toBe(10);
+
+    tick(500);
+    expect(counter).toBe(11);
   }));
 });
