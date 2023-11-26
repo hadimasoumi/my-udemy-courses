d91f135f390b753c1d27384f55bf065730639013
Hadi Masoumi <hadi.masoumi@kalemyazilim.com>
2023-11-27 01:13:49 +0300

    40. Mock an HTTP Backend using Cypress

--- angular-testing-course/cypress/integration/home.test.js
+++ angular-testing-course/cypress/integration/home.test.js
@@ -1,6 +1,11 @@
 describe("Home Page", () => {
   it("should display a list of courses", () => {
+    cy.fixture("courses.json").as("courseJSON");
+    cy.server();
+    cy.route("/api/courses", "@courseJSON").as("courses");
     cy.visit("/");
     cy.contains("All Courses");
+    cy.wait("@courses");
+    cy.get("mat-card").should("have.length", 9);
   });
 });
