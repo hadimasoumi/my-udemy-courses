c21b98c700950f1ecc781d0176e607171265445a
Hadi Masoumi <hadi.masoumi@kalemyazilim.com>
2023-11-26 19:44:46 +0300

    angular-testing-course: 9.using TestBed

--- angular-testing-course/src/app/courses/services/calculator.service.spec.ts
+++ angular-testing-course/src/app/courses/services/calculator.service.spec.ts
@@ -1,4 +1,6 @@
+import { TestBed } from '@angular/core/testing';
 import { CalculatorService } from './calculator.service';
+import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
   let loggerSpy: any;
@@ -6,7 +8,17 @@ describe('CalculatorService', () => {
 
   beforeEach(() => {
     loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-    calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {
+          provide: LoggerService,
+          useValue: loggerSpy,
+        },
+      ],
+    });
+
+    calculator = TestBed.inject(CalculatorService);
   });
 
   it('Should add two numbers', () => {
