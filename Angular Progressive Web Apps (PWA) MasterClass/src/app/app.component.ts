f4317b92a070d8f92d4540d61bc792ce15bb0c5c
Hadi Masoumi <hadi.masoumi@kalemyazilim.com>
2023-11-27 23:03:00 +0300

    pwa - 8. Service Worker Version Management - How Does it Work?

--- Angular Progressive Web Apps (PWA) MasterClass/src/app/app.component.ts
+++ Angular Progressive Web Apps (PWA) MasterClass/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SwPush, SwUpdate } from '@angular/service-worker';
+import { SwPush, SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { interval } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,19 @@ import { SwPush, SwUpdate } from '@angular/service-worker';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  constructor(private swUpdate: SwUpdate) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      interval(5 * 1000).subscribe(() => this.swUpdate.checkForUpdate().then(() => console.log('checking for updates')));
+    } else {
+      console.log('Service Worker on this browser is not enabled!');
+    }
+
+    this.swUpdate.versionUpdates.pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY')).subscribe(data => {
+      if (confirm('New version available. Load new version?')) {
+        window.location.reload();
+      }
+    });
+  }
 }
