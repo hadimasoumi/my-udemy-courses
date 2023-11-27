69ad020971862ec7ca56bf5d7581d8c0c44be11a
Hadi Masoumi <hadi.masoumi@kalemyazilim.com>
2023-11-27 13:51:53 +0300

    pwa - format codes

--- Angular Progressive Web Apps (PWA) MasterClass/src/app/lessons/lessons.component.ts
+++ Angular Progressive Web Apps (PWA) MasterClass/src/app/lessons/lessons.component.ts
@@ -1,31 +1,26 @@
-import {Component, OnInit} from '@angular/core';
-import {LessonsService} from "../services/lessons.service";
-import {Observable, of} from 'rxjs';
-import {Lesson} from "../model/lesson";
-import {SwPush} from "@angular/service-worker";
-import {catchError} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { LessonsService } from '../services/lessons.service';
+import { Observable, of } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { SwPush } from '@angular/service-worker';
+import { catchError } from 'rxjs/operators';
 
 @Component({
-    selector: 'lessons',
-    templateUrl: './lessons.component.html',
-    styleUrls: ['./lessons.component.css']
+  selector: 'lessons',
+  templateUrl: './lessons.component.html',
+  styleUrls: ['./lessons.component.css'],
 })
 export class LessonsComponent implements OnInit {
+  lessons$: Observable<Lesson[]>;
+  isLoggedIn$: Observable<boolean>;
 
-    lessons$: Observable<Lesson[]>;
-    isLoggedIn$: Observable<boolean>;
+  constructor(private lessonsService: LessonsService) {}
 
-    constructor(private lessonsService: LessonsService) {
+  ngOnInit() {
+    this.loadLessons();
+  }
 
-    }
-
-    ngOnInit() {
-        this.loadLessons();
-    }
-
-
-    loadLessons() {
-        this.lessons$ = this.lessonsService.loadAllLessons().pipe(catchError(err => of([])));
-    }
-
+  loadLessons() {
+    this.lessons$ = this.lessonsService.loadAllLessons().pipe(catchError(err => of([])));
+  }
 }
