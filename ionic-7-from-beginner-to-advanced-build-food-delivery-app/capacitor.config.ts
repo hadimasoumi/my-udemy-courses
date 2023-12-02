6512a477543bd530cc3053219845f1c57b889347
Hadi Masoumi <hadi.masoumi@kalemyazilim.com>
2023-12-02 13:36:38 +0300

    ionic - init project

---
+++ ionic-7-from-beginner-to-advanced-build-food-delivery-app/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic 7+ From Beginner to Advanced - Build Food Delivery App',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
