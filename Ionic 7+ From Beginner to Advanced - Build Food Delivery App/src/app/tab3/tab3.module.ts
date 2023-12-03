28e5a5a259bd748a65699229f9147a9f310a85a0
Hadi Masoumi <hadi.masoumi@kalemyazilim.com>
2023-12-03 13:58:31 +0300

    ionic - format project with prettier

--- Ionic 7+ From Beginner to Advanced - Build Food Delivery App/src/app/tab3/tab3.module.ts
+++ Ionic 7+ From Beginner to Advanced - Build Food Delivery App/src/app/tab3/tab3.module.ts
@@ -8,13 +8,7 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    Tab3PageRoutingModule
-  ],
-  declarations: [Tab3Page]
+  imports: [IonicModule, CommonModule, FormsModule, ExploreContainerComponentModule, Tab3PageRoutingModule],
+  declarations: [Tab3Page],
 })
 export class Tab3PageModule {}
