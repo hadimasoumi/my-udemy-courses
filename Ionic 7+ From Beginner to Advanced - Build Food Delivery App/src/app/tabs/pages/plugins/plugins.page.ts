6f508dd7449011cb5a64b043268e4f806fa52007
Hadi Masoumi <hadi.masoumi@kalemyazilim.com>
2023-12-04 17:06:21 +0300

    ionic - add camera

--- Ionic 7+ From Beginner to Advanced - Build Food Delivery App/src/app/tabs/pages/plugins/plugins.page.ts
+++ Ionic 7+ From Beginner to Advanced - Build Food Delivery App/src/app/tabs/pages/plugins/plugins.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-plugins',
   templateUrl: './plugins.page.html',
   styleUrls: ['./plugins.page.scss'],
 })
-export class PluginsPage implements OnInit {
+export class PluginsPage {
+  constructor() {}
 
-  constructor() { }
+  async share() {
+    // await Share.share({
+    //   title: 'See cool stuff',
+    //   text: 'Really awesome thing you need to see right meow',
+    //   url: 'http://ionicframework.com/',
+    //   dialogTitle: 'Share with buddies',
+    // });
 
-  ngOnInit() {
+    await Share.share({
+      url: 'http://ionicframework.com/',
+    });
   }
-
 }
