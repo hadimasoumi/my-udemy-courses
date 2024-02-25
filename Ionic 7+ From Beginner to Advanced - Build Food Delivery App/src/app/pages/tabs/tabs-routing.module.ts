7adaa04ca95bd2504611c8be05f2abeca0d77df1
Hadi Masoumi <hadi.masoumi@kalemyazilim.com>
2024-02-25 20:07:06 +0300

    ionic: 105. Setting up Tabs Layout & apply Theming in a Blank Project

--- Ionic 7+ From Beginner to Advanced - Build Food Delivery App/src/app/pages/tabs/tabs-routing.module.ts
+++ Ionic 7+ From Beginner to Advanced - Build Food Delivery App/src/app/pages/tabs/tabs-routing.module.ts
@@ -6,8 +6,31 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage
-  }
+    component: TabsPage,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/tabs/home'
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+      },
+      {
+        path: 'search',
+        loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+      },
+      {
+        path: 'cart',
+        loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
+      },
+      {
+        path: 'account',
+        loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
+      }
+    ]
+  },
 ];
 
 @NgModule({
