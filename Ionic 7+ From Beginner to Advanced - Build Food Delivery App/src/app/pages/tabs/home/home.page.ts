ec54c45cc41b90c1aa0c65ba4cce465763af4466
Hadi Masoumi <hadi.masoumi@kalemyazilim.com>
2024-02-25 20:46:46 +0300

    ionic: 106. Design Banners using Swiperjs (Landing Screen)

--- Ionic 7+ From Beginner to Advanced - Build Food Delivery App/src/app/pages/tabs/home/home.page.ts
+++ Ionic 7+ From Beginner to Advanced - Build Food Delivery App/src/app/pages/tabs/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IonicSlides} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  swiperModules = [IonicSlides];
 
   constructor() { }
 
